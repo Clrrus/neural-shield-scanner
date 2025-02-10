93af14a6eb6c3344521e45d4d0c8a95b903e8a77
Cem Erdem <cemerdem2006@gmail.com>
2025-02-10 22:33:55 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from port_scanner.port_scanner import run_scanner
 import time
-import subprocess
 from multiprocessing import Process
 from packet_sniffer.packet_sniffer import main as run_sniffer
 
@@ -20,17 +19,14 @@ def run_port_scanner():
 
 if __name__ == "__main__":
     try:
-        # Packet Sniffer'ı ayrı bir process olarak başlat
         sniffer_process = Process(target=run_sniffer)
         sniffer_process.start()
         
-        # Port Scanner'ı ana process'te çalıştır
         run_port_scanner()
         
     except KeyboardInterrupt:
         print("Main program stopping...")
     finally:
-        # Program sonlandığında sniffer process'ini temizle
         if 'sniffer_process' in locals():
             sniffer_process.terminate()
             sniffer_process.join()
