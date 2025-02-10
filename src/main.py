863cfe7d325ba0d5a1ef3e82632449e78219c365
Cem Erdem <cemerdem2006@gmail.com>
2025-02-10 19:34:55 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
 from port_scanner.port_scanner import run_scanner
+import time
 
 if __name__ == "__main__":
-    run_scanner()
+    while True:
+        try:
+            run_scanner()
+            time.sleep(3600)
+        except KeyboardInterrupt:
+            print("Exiting program...")
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+            time.sleep(5)
+            continue
